4ff941fa327142128471fbd37771800e6420941d
Laurence McFarlane <laurenceium@gmail.com>
2015-02-10 15:41:37 +1300

    Converted animations to css only

--- files/nonapp/index.js
+++ files/nonapp/index.js
@@ -10,32 +10,6 @@ $(function(){
     .css("bottom", 0-$(window).height())
     .css("margin-left", 0-$(window).height());
 
-  //Do the animation of the logos
-  var logoEl = $("#hero .logo");
-  var smalllogoEl = $("#hero .smalllogotext");
-  var mdsumEl = $("#hero .midsumtext");
-
-  var height = $(window).height();
-  var width = $(window).width();
-
-  logoEl.css("left", (width - logoEl.width())/2);
-  logoEl.css("top", 20);
-  mdsumEl.css("left", (width - mdsumEl.width())/2);
-  mdsumEl.css("top", (height - mdsumEl.height()));
-  smalllogoEl.css("left", (width - logoEl.width())/2);
-  smalllogoEl.css("top", 20);
-
-  logoEl.css("transition", "all 1s");
-  mdsumEl.css("transition", "all 1s");
-  smalllogoEl.css("transition", "all 1s");
-
-  mdsumEl.css("top", (height - mdsumEl.height())/2);
-
-  logoEl.css("left", width/2 - logoEl.width());
-  smalllogoEl.css("left", width/2);
-  smalllogoEl.css("opacity", 1);
-
-
   //Programme List javascript
   var wrapEl = $(".wrapper");
   var minHeight = $(".programmelist").outerHeight();
