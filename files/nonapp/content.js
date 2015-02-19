6852f0c3f2f0c4a2efdd41130fe344ccfb4705e3
Laurence McFarlane <laurenceium@gmail.com>
2015-02-19 13:22:58 +1300

    Moved js content into seperate file

---
+++ files/nonapp/content.js
@@ -0,0 +1,216 @@
+var people = {
+  "Antun Poljanich":{
+    image:"peoplephotos/antun.JPG", text:"<p>Antun Poljanich was born in Dubrovnik, Croatia, and studied music (piano and musical theory) at Dubrovnik's School of Music, &ldquo;Luka Sorkocevic&rdquo;. He went on to graduate in conducting from the Ljubljana School of Music in Slovenia and the Sarajevo School of Music in Bosnia and Herzegovina, after which he undertook his postgraduate studies in Graz, Austria and Leningrad (the present St Petersburg). In Leningrad he studied at the prestigious N. Rimsky Korsakov Conservatory for three years. During that time he conducted the N. Rimsky Korsakov Orchestra, Leningrad Symphony Orchestra and the Orchestra of Theatre Mally. </p><p>Upon his return to Croatia Antun conducted various European Orchestras: Slovene Philharmonic Orchestra (Slovenia), Zagreb Philharmonic Orchestra (Croatia), Veneto Philharmonia (Italy), Cadaques Symphony Orchestra (Spain), Miskolz Symphony Orchestra (Hungary) and many other orchestras and chamber groups of the region. He was also the permanent guest conductor of the Dubrovnik Symphony Orchestra in his hometown and worked as the conductor and assistant conductor of the Slovene Police Orchestra in Ljubljana. In 1986 he received the &ldquo;Lilly and Lovro von Matacic Young Conductor's Scholarship&rdquo; and Republic of Croatia scholarship for his studies in Russia. In 1993 he received the &ldquo;Oton Zupancic Award&rdquo; for his contribution to culture in Slovenia.</p> <p>After arriving in New Zealand in 1996, Antun regularly conducted the Auckland Philharmonia Orchestra and St Matthew's Chamber Orchestra. In 1998 he became Musical Director of the Auckland Youth Symphony Orchestra. Since 1998 he has travelled and performed with this group in many concerts throughout New Zealand and overseas. He recently took the orchestra on their first tours of mainland America and continental Europe.</p><p>He is also Director of Music at St Peter's College in Auckland, where he takes charge of the performance programme including the Concert Band, Symphony Orchestra, Vocal, Choir and the chamber music programme. Under his guidance these groups have been awarded regularly over the last 7-8 years for their achievements in regional, national and international competitions. He was awarded with the St Peter's College Scholarship in 2005 and the &ldquo;Kirk Burnard and Brown&rdquo; award for contribution to New Zealand music education. </p><p>In 2007, for the first time since his arrival in New Zealand, Antun returned to Dubrovnik, where once again he successfully conducted the Dubrovnik Symphony Orchestra, and in April 2009 he returned to Ljubljana to conduct the Slovene Philharmonia Orchestra.</p>"
+  },
+  "Harris Leung":{
+    image:"peoplephotos/harris.JPG", text:"Harris has been a member of the Auckland Youth Orchestra since 2013 and has previously played in the NZSO National Youth Orchestra. Previous teachers of Harris include John Thomson and Michael McLellan. He currently studies violin under Elizabeth Holowell. Outside of music his main interest is mathematics, and he devotes all his time to studying this at The University of Auckland."
+  },
+  "Gwyneth Nelmes":{
+    image:"peoplephotos/gwyneth.JPG", text:"Gwyneth Nelmes has been playing the violin for 11 years and is a student of Dimitri Atanassov. In 2013 she was a Michael Monaghan Scholar and last year was first violin in the Applachian Octet, which won the National Statham Award in the New Zealand Chamber Music Competition. She has played in many chamber groups and orchestras over the years, and has also participated in solo and group competitions. She loves to play music and has only just joined the AYO in 2015."
+  },
+  "Nanae Saga":{
+    image:"peoplephotos/nanae.JPG", text:"Nanae Saga studied violin in Robert-Schuman-Hochschule in Germany, playing in various orchestras including the Junge Deutsche Philharmonie, Junge Norddeutsche Philharmonie, Orchestre de Musicales de Programme Européen Jeunesse en Action and participated in several competitions. Nanae took part of Master Classes under Prof. Koji Toyoda, head of Suzuki Method, and Prof. Michael Gaiser."
+  },
+  "":{
+    image:"peoplephotos/.JPG", text:""
+  },
+}
+/*gallery = [
+    {
+        video: 'http://www.youtube.com/watch?v=gDnyn4rchgc',
+    },
+*/
+var galleryNoVid = [
+  {
+        image: 'galleryphotos/23.JPG',
+        title: 'Woodwind and Brasswind',
+        description: 'Auckland Town Hall',
+    },
+  {
+        image: 'galleryphotos/28.JPG',
+        title: 'Pizzicato cellos',
+        description: 'Auckland Town Hall',
+    },
+  {
+        image: 'galleryphotos/37.JPG',
+        title: 'Violas',
+        description: 'Auckland Town Hall',
+    },
+  {
+        image: 'galleryphotos/42.JPG',
+        title: 'Three Trumpeteers',
+        description: 'Auckland Town Hall',
+    },
+  {
+        image: 'galleryphotos/50.JPG',
+        title: 'Three Trumpeteers',
+        description: 'Auckland Town Hall',
+    },
+  {
+        image: 'galleryphotos/54.JPG',
+        title: 'In Rehearsal',
+        description: 'Auckland Town Hall',
+    },
+  {
+        image: 'galleryphotos/59.JPG',
+        title: 'Percussionists at work',
+        description: 'Auckland Town Hall',
+    },
+  {
+        image: 'galleryphotos/61.JPG',
+        title: 'Happy basses!',
+        description: 'Auckland Town Hall',
+    },
+  {
+        image: 'galleryphotos/72.JPG',
+        title: 'Cameron "Fastidious" Stuart',
+        description: 'Auckland Town Hall',
+    },
+  {
+        image: 'galleryphotos/92.JPG',
+        title: 'Rehearsal precision',
+        description: 'Auckland Town Hall',
+    },
+    {
+        image: 'galleryphotos/IMG_2963.JPG',
+        title: 'In Rehearsal',
+        description: 'Dunedin Town Hall',
+    },
+    {
+        image: 'galleryphotos/IMG_2976.JPG',
+        title: 'In Concert',
+        description: 'Dunedin Town Hall',
+    },
+    {
+        image: 'galleryphotos/IMG_2986.JPG',
+        title: 'First Violins',
+        description: 'Gore',
+    },
+    {
+        image: 'galleryphotos/IMG_3022.JPG',
+        title: 'Czardas played by Laurence McFarlane',
+        description: 'Te Anau',
+    },
+    {
+        image: 'galleryphotos/IMG_3032.JPG',
+        title: 'Enthusiastic Fans getting close up after the concert',
+        description: 'Te Anau',
+    },
+    {
+        image: 'galleryphotos/IMG_3044.JPG',
+        title: 'Cooking dinner for the orchestra on tour',
+        description: 'Te Anau Youth Hostel',
+        },
+    {
+        image: 'galleryphotos/IMG_3051.JPG',
+        title: 'Dinner and Games',
+        description: 'Te Anau Youth Hostel',
+        },
+
+    {
+        image: 'galleryphotos/IMG_3152.JPG',
+        title: 'Bungy Jumping undertaken by many!',
+        description: 'Queenstown',
+        },
+
+        {
+        image: 'galleryphotos/IMG_3185.JPG',
+        title: 'Robbie, Principal Cello, demonstrating',
+        description: 'Wanaka',
+        },
+
+        {
+        image: 'galleryphotos/IMG_3187.JPG',
+        title: 'In Rehearsal',
+        description: 'Wanaka',
+        },
+
+        {
+        image: 'galleryphotos/IMG_3188.JPG',
+        title: 'Almost full house',
+        description: 'Wanaka',
+        },
+    {
+        image: 'galleryphotos/IMG_3221.JPG',
+        title: 'Nick, Jono, and Robbie',
+        description: 'Gates of Haast',
+    },
+    {
+        image: 'galleryphotos/IMG_3223.JPG',
+        title: 'Carol and Jess',
+        description: 'Gates of Haast',
+    },
+    {
+        image: 'galleryphotos/IMG_3240.JPG',
+        title: 'Made it to Fox Glacier',
+        description: 'Fox Glacier',
+    },
+    {
+        image: 'galleryphotos/IMG_3260.JPG',
+        title: 'Cooking dinner',
+        description: 'Noahs Arc Hostel, Greymouth',
+    },
+    {
+        image: 'galleryphotos/IMG_3300.JPG',
+        title: 'Beautiful basses',
+        description: 'Westport',
+    },
+    {
+        image: 'galleryphotos/IMG_3303.JPG',
+        title: 'Photogenic Horns',
+        description: 'Westport',
+        },
+    {
+        image: 'galleryphotos/IMG_3304.JPG',
+        title: 'Woodwinds, deep in concentration',
+        description: 'Westport',
+        },
+
+    {
+        image: 'galleryphotos/IMG_3329.JPG',
+        title: 'In Concert',
+        description: 'Nelson School of Music',
+        },
+
+        {
+        image: 'galleryphotos/IMG_3331.JPG',
+        title: 'Czardas',
+        description: 'Nelson School of Music',
+        },
+
+        {
+        image: 'galleryphotos/IMG_3343.JPG',
+        title: 'Back home, In Rehearsal',
+        description: 'Auckland Town Hall',
+        },
+
+        {
+        image: 'galleryphotos/IMG_3353.JPG',
+        title: 'In Concert',
+        description: 'Auckland Town Hall',
+        },
+
+
+];
+var posters = [
+  {image:"historyphotos/destiny.jpg",    title:"Destiny",    description:"September-October 2014</em><br /><br />    Verdi La forza del destino (Overture)<br />    Bruch Violin Concerto No. 1<br />    Beethoven Symphony No. 5 <br /><br />    <em>Antun Poljanich, Jim Wu<br />    Howick, Massey, Whitianga, Auckland</em>"},
+  {image:"historyphotos/heartstrings.jpg",    title:"Heartstrings",    description:"<em>July-August 2014</em><br /><br />    Dvo&#345;&aacute;k Carnival Overture<br />    Schumann Cello Concerto<br />    Sibelius Symphony No. 1 <br /><br />    <em>Antun Poljanich, Edward King<br />    Taupo, Morrinsville, Whangaparaoa, Auckland</em>"},
+  {image:"historyphotos/folklore.jpg",    title:"Folklore",    description:"<em>April-May 2014</em><br /><br />    Brahms Hungarian Dances<br />    Ligeti Concert Rom&acirc;nesc<br />    Bartok Hungarian Sketches<br />    Bartok Viola Concerto<br />    Kod&aacute;ly Dances of Gal&aacute;nta<br /><br />    <em>Antun Poljanich, Alex McFarlane<br />    Auckland, Dargaville, Whangarei, Whangaparaoa</em>"},
+  {image:"historyphotos/titan.jpg",    title:"TITAN",    description:"<em>October 2013</em><br /><br />    Thomas Press crashing through a bright mist<br />    Mahler Symphony No. 1 'Titan'<br /><br />    <em>Antun Poljanich<br />    Auckland</em>"},
+  {image:"historyphotos/tot.jpg",    title:"Tchaikovsky on Tour",    description:"<em>June-July 2013</em><br /><br />    Rimsky-Korsakov Capriccio Espagnole<br />    Monti Cs&aacute;rd&aacute;s<br />    Saint-Sa&euml;ns Havanaise<br />    Tchaikovsky Symphony No. 4<br /><br />    <em>Antun Poljanich, Laurence McFarlane, Richard Chen<br />    Auckland, Gore, Te Anau, Westport</em>"},
+  {image:"historyphotos/lifehero.jpg",    title:"Life of a Hero",    description:"<em>April-May 2013</em><br /><br />    Hindemith Trauermusik<br />    Mahler Todtenfeier<br />    Beethoven Violin Concerto<br /><br />    <em>Antun Poljanich, Olivia Francis<br />    Auckland, Whangarei, Whitianga</em>"},
+  {image:"historyphotos/russialove.jpg",    title:"From Russia With Love (An Orchestral Celebration)",    description:"<em>September-October 2012</em><br /><br />    Mozart Marriage of Figaro Overture<br />    Robbie Ellis In meinem letzten Leiden<br />    Guilmant Symphony No. 1 for Organ and Orchestra in D minor (Auckland only)<br />    Shostakovich Symphony No. 9<br /><br />    <em>Birkenhead, Whangarei, Helensville, Auckland</em>"},
+  {image:"historyphotos/beethoven9.jpg",    title:"Beethoven 9",    description:"<em>July 2012</em><br /><br />    Beethoven Symphony No. 9<br /><br />    <em>Antun Poljanich, Isabella Moore, Anthony Schneider, Elisha Fa'i-Hulton, Amitai Pati<br />    Auckland, Papakura, Tauranga, Taupo, Whangarei, Whitianga</em>"},
+  {image:"historyphotos/fantasy.jpg",    title:"Fantasy",    description:"<em>March/April 2012</em><br /><br />    Rimsky-Korsakov Scheherazade <br />    Vaughan-Williams Fantasia on a Theme by Thomas Tallis <br />    Mozart Bassoon Concerto<br /><br />    <em>Antun Poljanich, Albee Ai<br />    Auckland, Whangarei, Warkworth, Helensville</em>"},
+  {image:"historyphotos/masterclef.jpg",    title:"MasterClef",    description:"<em>October 2011</em><br /><br />    Elgar Serenade for Strings<br />    Weber Clarinet Concerto No. 2<br />    Beethoven Symphony No. 4<br /><br />    <em>Antun Poljanich, Natalie Harris<br />    Auckland, Warkworth, Thames, Whangarei</em>"},
+  {image:"historyphotos/europa.jpg",    title:"Europa / AYO European Tour",    description:"<em>July-August 2011</em><br /><br />    Mendelssohn Hebrides Overture<br />    Strauss Oboe Concerto<br />    Alex Taylor between<br />    Poulenc Sinfonietta    <br /><br />    <em>Antun Poljanich, Thomas Hutchinson<br />    Howick, Helensville, Whangarei, Auckland<br />    Germany, Austria, Slovenia</em>"},
+  {image:"historyphotos/nyny.jpg",    title:"New York, New York",    description:"<em>May 2011</em><br /><br />    <em>Antun Poljanich, Tim Beveridge, Natalie Harris<br />    Auckland, Kerikeri, Orewa</em>"},
+  {image:"historyphotos/inlux.jpg",    title:"In Lux Aeterna / In Eternal Light",    description:"<em>March-April 2011</em><br /><br />    Part Cantus in Memory of Benjamin Britten<br />    Mozart Piano Concerto No. 23 in A major<br />    Tchaikovsky Symphony No. 5 in e minor<br /><br />    <em>Antun Poljanich, Jane Sohn<br />    Auckland, Helensville, Hamilton, Whitianga</em>"},
+  {image:"historyphotos/ussr.jpg",    title:"Back to the USSR",    description:"<em>September-October 2010</em><br /><br />    Khachaturian Gayane Suite<br />    Prokofiev Piano Concerto No. 1<br />    Shostakovich Symphony No. 5<br /><br />    <em>Antun Poljanich, Jason Bae<br />    Auckland, Tauranga, Hamilton, Whangarei, Warkworth</em>"},
+  {image:"historyphotos/yourmajesty.jpg",    title:"Your Majesty",    description:"<em>June-July 2010</em><br /><br />    Mandeno Phantasmagoria<br />    Bruch Violin Concerto No. 1<br />    Mozart Clarinet Concerto<br />    Elgar Enigma Variations<br /><br />    <em>Peter Thomas, Richard Chen, Rowan Meade<br />    Auckland, Katikati, Cambridge</em>"},
+  {image:"historyphotos/meetigor.jpg",    title:"Meet Igor",    description:"<em>February-March 2010</em><br /><br />    Albinoni Oboe Concerto<br />    Mandeno Typhon<br />    Stravinsky Arioso, Ebony Concerto, Suites for Small Orchestra, Pulcinella Suite<br /><br />    <em>Antun Poljanich, Thomas Hutchinson<br />    Auckland, Orewa, New Lynn</em>"},
+  {image:"historyphotos/fireice.jpg",    title:"Fire and Ice",    description:"<em>October-November 2009</em><br /><br />    Glinka Ruslan & Ludmilla Overture<br />    Allen Fantasia<br />    Tchaikovsky Romeo & Juliet Overture<br />    Sibelius Symphony No. 3<br /><br />    <em>Antun Poljanich, Thomas Hutchinson<br />    Auckland, Orewa, New Lynn</em>"},
+  {image:"historyphotos/diamondjubilee.jpg",    title:"Diamond Jubilee Concert",    description:"<em>August-October 2008</em><br /><br />    J Strauss Die Fledermaus Overture<br />    Cowan Legend of the Trojan Bird<br />    Ravel Piano Concerto in g minor<br />    Tchaikovsky Symphony No. 6<br /><br />    <em>Antun Poljanich, John-Paul Muir<br />    Los Angeles, New York, Rhode Island, Boston, Auckland</em>"},
+  {image:"historyphotos/symphonicheroes.jpg",    title:"Symphonic Heroes",    description:"<em>March-April 2008</em><br /><br />    Berlioz Roman Carnival Overture<br />    Aratunian Trumpet Concerto<br />    Beethoven Symphony No. 7<br /><br />    <em>Antun Poljanich, Bill Stoneham<br />    Auckland, Warkworth, Orewa, Thames</em>"},
+  {image:"historyphotos/kingdom.jpg",    title:"Kingdom of the Animals",    description:"<em>April 2008</em><br /><br />    Prokofiev Peter and the Wolf <br />    Saint-Saens Carnival of the Animals <br />    Rimsky Korsakov Flight of the Bumble Bee<br />    Tchaikovsky Prelude to Swan Lake <br />    Mussorgsky Chicks in their shells (from Pictures at an Exhibition)<br />    Alex Taylor Noughts and Crosses <br /><br />    <em>Antun Poljanich, Jason Bae, Joong-Han Jung<br />    Warkworth, Auckland</em>"},
+];
