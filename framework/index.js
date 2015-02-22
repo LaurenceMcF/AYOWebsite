ae6c1df45d8484d84955ffb273da599d76c2b210
Laurence McFarlane <laurenceium@gmail.com>
2015-02-23 10:42:28 +1300

    Modififed styling of current season and top logo

--- framework/index.js
+++ framework/index.js
@@ -5,13 +5,13 @@ $(function(){
   var adjMenuOpacity = function(){
     if ($(window).scrollTop() > height) {
       $('.menu').css("background-color", 'rgba(161, 20, 20, 0.8)');
-      $('.menu .logo-container').css("width", 236);
+      //$('.menu .logo-container').css("width", 150);
     } else {
       $('.menu').css("background-color", 'rgba(161, 20, 20, 0)');
       var opacity = $(window).scrollTop() / height * 0.8;
-      var logosize = 436 - $(window).scrollTop() / height * 200;
+      var logosize = 250 - $(window).scrollTop() / height * 100;
       $('.menu').css("background-color", 'rgba(161, 20, 20, '+opacity+')');
-      $('.menu .logo-container').css("width", logosize);
+      //$('.menu .logo-container').css("width", logosize);
     }
   }
   $(window).scroll(adjMenuOpacity);
