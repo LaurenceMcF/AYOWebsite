e470854b7d6680b07da7c009ee51329eee31ed37
Laurence McFarlane <laurenceium@gmail.com>
2015-02-22 13:46:26 +1300

    Removed unneeded code

--- framework/index.js
+++ framework/index.js
@@ -3,9 +3,6 @@ $(function(){
   //Gallery Creation
   $(".gallery").each(function(index, el){
     var $el = $(el);
-    //Set height when we can
-    //$el.css("height",
-    //  $el.children(".gallery-img-container").children(".current").height());
     $el.children(".prev, .next").click(function(){
 
       var current = $el.children(".gallery-img-container").children(".current");
@@ -24,7 +21,6 @@ $(function(){
       }
       future.addClass("current").css("opacity", 1);
       current.removeClass("current").css("opacity", 0);
-      $el.css("height", future.height());
     });
   });
 
